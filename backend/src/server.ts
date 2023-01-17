1b64aa2cb02478c565871e4c8b074532dfb29fde
Adriano <adriano_alves_2008@hotmail.com>
2023-01-17 15:40:57 -0300

    Utilizando o prisma

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
+import {PrismaClient} from '@prisma/client'
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
 app.get('/', ()=>{
   return 'Oi 5'
@@ -8,4 +10,6 @@ app.get('/', ()=>{
 
 app.listen({
   port:3333
+}).then(()=>{
+  console.log('Servidor rodando')
 })
