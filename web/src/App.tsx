90a4ef1b4393f401f10ff89c094054d98c930b10
Adriano <adriano_alves_2008@hotmail.com>
2023-01-18 21:32:42 -0300

    tabela de hábitos diários

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,14 @@
+import { Header } from './components/Header'
+import SummaryTable from './components/SummaryTable'
 import './styles/global.css'
 // import Habit from './components/Habit'
-
-import logoImage from './assets/logo.svg'
-import { Plus } from 'phosphor-react'
 
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-        <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
-          <img src={logoImage} alt="Logo" />
-
-          <button
-            type="button"
-            className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
-          >
-            <Plus size={20} className="text-violet-500" />
-            Novo hábito
-          </button>
-        </div>
+        <Header />
+        <SummaryTable />
       </div>
     </div>
   )
