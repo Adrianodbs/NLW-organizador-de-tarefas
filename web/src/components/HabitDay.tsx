005652fef1423a4948ae216641c9bc48020ed112
Adriano <adriano_alves_2008@hotmail.com>
2023-01-25 21:12:03 -0300

    buscando resumo da API

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -5,12 +5,14 @@ import clsx from 'clsx'
 import { Check } from 'phosphor-react'
 
 interface HabitProps {
-  completed: number
-  amount: number
+  date: Date
+  completed?: number
+  amount?: number
 }
 
-export default function HabitDay({ completed, amount }: HabitProps) {
-  const completedPercentage = Math.round((completed / amount) * 100)
+export default function HabitDay({ completed = 0, amount = 0 }: HabitProps) {
+  const completedPercentage =
+    amount > 0 ? Math.round((completed / amount) * 100) : 0
   return (
     <Popover.Root>
       <Popover.Trigger
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,5 +1,15 @@
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
 import HabitDay from './HabitDay'
+import { useEffect, useState } from 'react'
+import { api } from '../lib/axios'
+import dayjs from 'dayjs'
+
+type Summary = {
+  id: string
+  date: string
+  amount: number
+  completed: number
+}[]
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
@@ -9,6 +19,14 @@ const minumumSummaryDatesSize = 18 * 7
 const amountOfDaysToFill = minumumSummaryDatesSize - summaryDates.length
 
 export default function SummaryTable() {
+  const [summary, setSummary] = useState<Summary>([])
+
+  useEffect(() => {
+    api.get('summary').then(response => {
+      setSummary(response.data)
+    })
+  }, [])
+
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -26,11 +44,15 @@ export default function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map(date => {
+          const dayInSummary = summary.find(day => {
+            return dayjs(date).isSame(day.date, 'day')
+          })
           return (
             <HabitDay
-              amount={5}
-              completed={Math.round(Math.random() * 5)}
               key={date.toString()}
+              date={date}
+              amount={dayInSummary?.amount}
+              completed={dayInSummary?.completed}
             />
           )
         })}
